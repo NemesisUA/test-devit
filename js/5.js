114e5cf337c81f755c46b8c66fb8b7dd2c9f9dde
NemesisUA <display.n0ne.01.gmail.com>
2024-04-25 16:34:00 +0300

    feat: js task #5

---
+++ js/5.js
@@ -0,0 +1,25 @@
+function NotificationException() { }
+function ErrorException() { }
+
+function primitiveMultiply(a, b) {
+  const rand = Math.random();
+  if (rand < 0.5) {
+    return a * b;
+  } else if (rand > 0.85) {
+    throw new ErrorException()
+  } else {
+    throw new NotificationException()
+  }
+}
+
+function reliableMultiply(a, b) {
+  while (true) {
+    try {
+      return primitiveMultiply(a, b);
+    } catch (e) {
+      if (e instanceof ErrorException) {
+        throw e;
+      }
+    }
+  }
+}
