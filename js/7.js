806efeb2962379aaf7d4ef2b7765fcaaa0a22ff7
NemesisUA <display.n0ne.01.gmail.com>
2024-04-26 15:05:08 +0300

    feat: js task #7

---
+++ js/7.js
@@ -0,0 +1,13 @@
+function getBase64FromUrl(url) {
+  return fetch(url)
+    .then(response => response.blob())
+    .then(blob => new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onloadend = () => resolve(reader.result);
+      reader.onerror = reject;
+      reader.readAsDataURL(blob);
+    }));
+}
+// getBase64FromUrl('https://lh3.googleusercontent.com/i7cTyGnCwLIJhT1t2YpLW-zHt8ZKalgQiqfrYnZQl975-ygD_0mOXaYZMzekfKW_ydHRutDbNzeqpWoLkFR4Yx2Z2bgNj2XskKJrfw8')
+//   .then(console.log)
+//   .catch(console.error);
